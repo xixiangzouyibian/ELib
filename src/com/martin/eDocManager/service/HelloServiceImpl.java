e191386110efbef960d87d29dac4223316ff0fcc
Martin <zmbeatit@163.com>
2020-05-26 21:49:51 +0800

    Testing AOP
    Signed-off-by: Martin <zmbeatit@163.com>

---
+++ src/com/martin/eDocManager/service/HelloServiceImpl.java
@@ -0,0 +1,23 @@
+package com.martin.eDocManager.service;
+
+import com.martin.eDocManager.annotation.MethodDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class HelloServiceImpl implements HelloService {
+
+    @Override
+    @MethodDetails
+    public String sayHello(String message) {
+        try {
+            // mock other business logic doing
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "Hello Service is saying: " + message;
+    }
+}
